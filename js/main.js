e8c7e79722fa0277ed0e3a182b55c86f20041210
Jamie Humphries <jamie.humphries@outlook.com>
2019-11-30 19:23:07 +0000

    Terrakion is availble.

--- js/main.js
+++ js/main.js
@@ -794,7 +794,7 @@ const PokedexService = (function () {
       // { id: '636', name: 'Larvesta' },
       // { id: '637', name: 'Volcarona' },
       { id: '638', name: 'Cobalion' },
-      // { id: '639', name: 'Terrakion' },
+      { id: '639', name: 'Terrakion' },
       // { id: '640', name: 'Virizion' },
       // { id: '641', name: 'Tornadus' },
       // { id: '642', name: 'Thundurus' },
