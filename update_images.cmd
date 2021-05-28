4c4fc8369e7fba5104a42f9d5bfc3a1e4a7fcebb
Jamie Humphries <jamie.humphries@softwire.com>
2021-05-28 10:15:22 +0100

    Automated image updates.

---
+++ update_images.cmd
@@ -0,0 +1,7 @@
+set /p commit_hash=<pogo_assets_commit_hash.txt
+cd ..\pogo_assets
+git pull
+git diff %commit_hash% HEAD --name-only -- ".\Images\Pokemon - 256x256" | xargs -d "\n" cp -t ..\godex\images\
+cd ..\godex
+git rev-parse HEAD > pogo_assets_commit_hash.txt
+git status
