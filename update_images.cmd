c07fabe04ae1717b3f3f047b4e634822a435f568
Jamie Humphries <jamie.humphries@outlook.com>
2021-06-05 20:34:05 +0100

    Update image script.

--- update_images.cmd
+++ update_images.cmd
@@ -4,5 +4,5 @@ git pull
 git diff %commit_hash% HEAD --name-only -- ".\Images\Pokemon - 256x256" | xargs --delimiter "\n" --no-run-if-empty cp --target-directory "..\godex\images"
 git rev-parse HEAD > ..\godex\pogo_assets_commit_hash.txt
 cd ..\godex
-git add images
+git add images pogo_assets_commit_hash.txt
 git status
